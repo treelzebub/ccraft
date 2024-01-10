70517600a8d6ff240740f3541993b14f1aaed817
treelzebub <treelzebub@users.noreply.github.com>
2024-01-09 16:47:36 -0800

    count

--- count_chest_items.lua
+++ count_chest_items.lua
@@ -4,19 +4,33 @@ require "tables"
 local filename = "count.txt"
 local from_disk = get_lines(filename)
 
+local printer = peripherals.find("printer")
+local monitor = peripherals.find("monitor")
+local chest = peripherals.find("minecraft:chest")
+
 local table = {
-    ["minecraft:coal_ore"] = "Coal Ore";
-    ["minecraft:iron_ore"] = "Iron Ore";
-    ["minecraft:gold_ore"] = "Gold Ore";
-    ["minecraft:copper_ore"] = "Copper Ore";
-    ["minecraft:redstone_ore"] = "Redstore Ore";
-    ["modern_industrialization:lignite_coal_ore"] = "Lignite Coal Ore";
-    ["modern_industrialization:tin_ore"] = "Tin Ore";
-    ["techreborn:silver_ore"] = "Silver Ore";
-    ["create:zinc_ore"] = "Zinc Ore";
-    [""] = "";
-    [""] = "";
-    [""] = "";
+  ["minecraft:coal_ore"] = "Coal";
+  ["minecraft:iron_ore"] = "Iron";
+  ["minecraft:gold_ore"] = "Gold";
+  ["minecraft:copper_ore"] = "Copper";
+  ["minecraft:redstone_ore"] = "Redstore";
+  ["modern_industrialization:lignite_coal_ore"] = "Lignite Coal";
+  ["modern_industrialization:tin_ore"] = "Tin";
+  ["techreborn:silver_ore"] = "Silver";
+  ["create:zinc_ore"] = "Zinc";
 }
 
-print("Success!")
+local function add(item)
+  monitor.write(table[item.name] .. ": " .. item.count)
+end
+
+monitor.setCursorPos(1,1)
+
+for slot, item in pairs(chest.items())
+  local table = chest.items()
+  if has_key(table, item.name) then
+    add(item)
+  else
+    printer.write("Add to table: " .. item.name)
+  end
+end
